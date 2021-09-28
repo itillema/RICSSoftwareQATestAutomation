fb832a879b6f29637bf6082601d26c28a48a9d9c
Isaac Tillema <Tillema97@gmail.com>
2021-09-28 16:22:42 -0400

    Changed template file name in TestAutomation project

---
+++ RICS Software QA/TestingTemplate.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+
+namespace RICS_Software_QA
+{
+    public class Tests
+    {
+        // AAA ("Triple A paradigm"):
+        // Arrange (Setup)  Everything you'll need to setup the test
+        // Act   (Test)  Running the method or making an action
+        // Assert (Test)  Checking for a value to validate and pass the test
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1() //Test case names should exaplain what you're testing and what should happen... e.g.: CreateATitle_ShouldSetTitle(){}
+        {
+            Assert.Pass(); //See Assert definition to see what can be checked for with the NUnit class
+        }
+    }
+}
