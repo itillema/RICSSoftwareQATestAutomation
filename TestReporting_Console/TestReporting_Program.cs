42477b215f6c417f800dd814949b4693db4ecc91
Isaac Tillema <Tillema97@gmail.com>
2021-09-28 16:02:24 -0400

    Added Console and Repository projects

---
+++ TestReporting_Console/TestReporting_Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestReporting_Console
+{
+    class TestReporting_Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
