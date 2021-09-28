fed1cd097ef8495ec5afab1b0b74ed376004e5e9
Isaac Tillema <Tillema97@gmail.com>
2021-09-28 15:57:24 -0400

    Add project files.

---
+++ RICS Software QA/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace RICS_Software_QA
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
