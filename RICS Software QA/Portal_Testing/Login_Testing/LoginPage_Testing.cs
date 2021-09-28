26c375e5659081aaa78716ae9cab8c3702fe5782
Isaac Tillema <Tillema97@gmail.com>
2021-09-28 16:50:07 -0400

    completed Portal testing suite

---
+++ RICS Software QA/Portal_Testing/Login_Testing/LoginPage_Testing.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace RICS_Software_QA
+{
+    public class LoginPage_Testing
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1() 
+        {
+            Assert.Pass(); 
+        }
+    }
+}
